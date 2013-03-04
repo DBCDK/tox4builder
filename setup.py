8cae400614b2e41bd9cbcb8f9eaf37d2739be1a9
Michael Carlsson <mca@dbc.dk>
2013-03-04 13:13:54 +0100

    Initial tox configuration.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name = 'jjbuilder',
+  version = 'dev',
+  describtion = 'Creating, uploading, and delete of jobs to a Jenkins CI server based on scripts which can be invoked from commandline.',
+  packages = ['jjbuilder']
+  )
