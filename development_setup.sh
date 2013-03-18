a5f929cb904d26a89fb85aa45a21426eb9e6d59d
Michael Carlsson <mca@dbc.dk>
2013-03-18 12:42:45 +0100

    Download script checks out develop branch, sets up tracking of master branch, and initialized git-flow.

--- development_setup.sh
+++ development_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -o errexit
 
-git clone git@github.com:DBCDK/jjbuilder.git
+git clone -b develop git@github.com:DBCDK/jjbuilder.git
 pushd jjbuilder > /dev/null
+git branch --track master
 git flow init -d
 popd > /dev/null
 
